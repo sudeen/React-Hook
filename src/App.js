352dad246421921de6e84aeec1e0c2af60f1d9a1
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-12 00:29:39 +1100

    controlling useEffect behaviour for searching

--- src/App.js
+++ src/App.js
@@ -5,25 +5,33 @@ const App = () => {
   // state
   const [news, setNews] = useState([])
   const [searchQuery, setSearchQuery] = useState('react')
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react')
+
   // fetch news 
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(error => console.log(error))
   }
   useEffect(() => {
     fetchNews()
-  }, [searchQuery]) // you can control the behaviour of useEffect using the second argument
+    // eslint-disable-next-line
+  }, [url]) // you can control the behaviour of useEffect using the second argument
 
 const handleChange = (e) => {
   setSearchQuery(e.target.value)
 }
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+}
+
   return(
     <div>
       <h2>News</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" value={searchQuery} onChange={handleChange}/>
         <button>Search</button>
       </form>
